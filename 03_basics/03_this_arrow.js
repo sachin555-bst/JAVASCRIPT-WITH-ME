cf6793b172b8e60e3d92062ccddf81898d266d19
sachin555-bst <vtu18742@veltech.edu.in>
2024-02-15 22:46:44 +0530

    this and arrow

---
+++ 03_basics/03_this_arrow.js
@@ -0,0 +1,65 @@
+const user ={
+    username :"sachin",
+    price :"555",
+
+    welcomemessage: function(){
+        console.log(`${this.username} ,welcome to website`)
+    }
+    
+}
+
+// user.welcomemessage()
+// user.username = "Sac"
+// user.welcomemessage()
+
+// console.log(this)  //output : {}
+
+
+
+// function root(){
+//     let username ="sachin"
+
+//     console.log(this)  //all the values...global 
+// }
+// root()
+
+function root(){
+    let username ="sachin"
+    //console.log(this.username)
+}
+root()
+
+const root1= function(){
+    let username ="sachibn"
+    console.log(this.username)  //this doesnot work inside the function
+    
+}
+root1()
+
+//ARROW function
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);      //undefined
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {   //curly brackets means use return
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2   //impicit arrow
+
+// const addTwo = (num1, num2) => ( num1 + num2 )   //after arrow if small braket means no return
+
+const addTwo = (num1, num2) => ({username: "hitesh"})  
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
